14879a88600eebbff6a09e4aafcfb8068d5d300a
Rachel Dao <rachelqdao@gmail.com>
2024-02-20 13:02:20 -0500

    fixed arrObj

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './qcomps/artistsRemoveArr'
+import Profile from './qcomps/arrObj'
 
 export default function Home() {
   return (
